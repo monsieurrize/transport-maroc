fc5090ba16aca1d317f9b3cc51e4cf36d1d6b883
monsieurrize <monsieurrize@gmail.com>
2026-06-09 21:21:45 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Transport Maroc-Europe',
+        short_name: 'TransMaroc',
+        description: 'Mise en relation transporteurs et clients Maroc Europe',
+        theme_color: '#C1440E',
+        background_color: '#0F1B2D',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
+})
