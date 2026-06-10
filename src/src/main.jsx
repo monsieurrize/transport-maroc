20bfbf0f96dfbdf2aa7025cf560451d11bdaf32b
monsieurrize <monsieurrize@gmail.com>
2026-06-10 16:49:19 +0200

    Initialize main.jsx with routing and authentication

---
+++ src/src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider, useAuth } from './components/AuthProvider'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import NewAnnouncement from './pages/NewAnnouncement'
+import AnnouncementDetail from './pages/AnnouncementDetail'
+import Profile from './pages/Profile'
+import './index.css'
+
+function PrivateRoute({ children }) {
+  const { user, loading } = useAuth()
+  if (loading) return <div className="loading-screen"><div className="spinner" /></div>
+  return user ? children : <Navigate to="/login" replace />
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path="/nouvelle-annonce" element={<PrivateRoute><NewAnnouncement /></PrivateRoute>} />
+          <Route path="/annonce/:id" element={<PrivateRoute><AnnouncementDetail /></PrivateRoute>} />
+          <Route path="/profil" element={<PrivateRoute><Profile /></PrivateRoute>} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
